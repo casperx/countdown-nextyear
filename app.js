f433b670ca45e0f7d59f562c00617354b43d65da
Ruangvit Sriumphaiviwat <casperx102@gmail.com>
2021-06-29 11:04:24 +0700

    add percent complete of year

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const result = document.querySelector('#result');
+const percent = document.querySelector('#percent');
 const now = new Date();
-const offset = now.getTimezoneOffset() * 60000;
-let next = new Date(now.getFullYear() + 1, 0, 1, 0, 0, 0);
-const update = () => {
+const offset = now.getTimezoneOffset() * 6e4;
+const curYear = now.getFullYear();
+const next = new Date(curYear + 1, 0, 1, 0, 0, 0);
+const prev = new Date(curYear, 0, 1, 0, 0, 0);
+const yearDuration = next - prev;
+const update = (dt) => {
     const now = new Date();
-    const datetime = new Date(next - now + offset);
+    const duration = next - now + offset;
+    const progress = (1 - duration / yearDuration) * 100;
+    const datetime = new Date(duration);
     result.textContent = 
         datetime.getMonth() + ' month ' + 
         (datetime.getDate() - 1) + ' day ' + 
         datetime.getHours() + ' hour ' + 
         datetime.getMinutes() + ' minute ' + 
         datetime.getSeconds() + ' second';
-    setTimeout(update, 500);
+    percent.textContent = 'That is, ' + curYear + ' is ' + progress.toFixed(6) + '% complete';
+    requestAnimationFrame(update);
 };
 update();
